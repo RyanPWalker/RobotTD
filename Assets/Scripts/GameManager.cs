ad1866ca016a091829642c133b1b9c167dfa8cfa
Ryan Walker <rywalker@overstock.com>
2020-05-27 08:36:47 -0600

    5.6 Added Currency

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManager : Singleton<GameManager>
 {
     public TowerBtn ClickedBtn { get; set; }
 
+    private int currency;
+
+    [SerializeField]
+    private Text currencyText;
+
+    public int Currency
+    {
+        get
+        {
+            return currency;
+        }
+        set
+        {
+            this.currency = value;
+            this.currencyText.text = "<color=lime>$</color>" + value.ToString();
+        }
+    }
+
     // Use this for initialization
     void Start () {
-
+        Currency = 5;
 	}
 
 	// Update is called once per frame
