833e98288d7d541569519629ac5ae3f2392bcc4a
Nick Chatelain <nickdchatelain@gmail.com>
2020-07-13 15:38:20 -0600

    chapter 3.1

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -10,15 +10,19 @@ public class LevelManager : MonoBehaviour
 
     [SerializeField]
     private CameraMovement cameraMovement;
-    
-    public float TileSize{
-        get {return  tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+
+    public Dictionary<Point, TileScript> Tiles { get; set; }
+
+    public float TileSize
+    {
+        get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //Executes the create level function
         CreateLevel();
     }
 
@@ -26,39 +30,47 @@ public class LevelManager : MonoBehaviour
     void Update()
     {
 
-    }
-    
+    } 
+
     private void CreateLevel()
     {
+        Tiles = new Dictionary<Point, TileScript>();
         string[] mapData = ReadLevelText();
         int mapX = mapData[0].ToCharArray().Length;
         int mapY = mapData.Length;
-        Vector3 maxTile= Vector3.zero;
+        Vector3 maxTile = Vector3.zero;
         Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
         for (int y = 0; y < mapY; y++)
         {
             char[] newTiles = mapData[y].ToCharArray();
             for (int x = 0; x < mapX; x++)
             {
-               maxTile = PlaceTile(newTiles[x].ToString(), x, y, worldStart);
+                PlaceTile(newTiles[x].ToString(), x, y, worldStart);
             }
         }
-        cameraMovement.SetLimits(new Vector3(maxTile.x+ TileSize, maxTile.y -TileSize)); 
+
+        maxTile = Tiles[new Point(mapX - 1, mapY - 1)].transform.position;
+
+        cameraMovement.SetLimits(new Vector3(maxTile.x + TileSize, maxTile.y - TileSize));
     }
-    
-    public Vector3 PlaceTile(string tileType, int x, int y, Vector3 worldStart)
+
+    public void PlaceTile(string tileType, int x, int y, Vector3 worldStart)
     {
+        //parses tile
         int tileIndex = int.Parse(tileType);
-        GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
-        newTile.transform.position = new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y));
-        return newTile.transform.position;
+        //creates tile
+        TileScript newTile = Instantiate(tilePrefabs[tileIndex]).GetComponent<TileScript>();
+        //uses  new tile
+
+        newTile.Setup(new Point(x, y), new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y)));
+        Tiles.Add(new Point(x, y), newTile);
     }
 
     private string[] ReadLevelText()
     {
         TextAsset bindData = Resources.Load("Level") as TextAsset;
-        string data = bindData.text.Replace(Environment.NewLine, string.Empty); 
-        return data.Split('-'); 
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+        return data.Split('-');
     }
 
 }
