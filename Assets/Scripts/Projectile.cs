94f4b988372af9b257880089bcf9e5282b279fdd
Ryan Walker <rywalker@overstock.com>
2021-01-02 15:05:13 -0700

    10.2 added puff animation, projectile animations, and triggers.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,10 +8,12 @@ public class Projectile : MonoBehaviour
 
     private Tower parent;
 
+    private Animator myAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -50,7 +52,7 @@ public class Projectile : MonoBehaviour
             if (target.gameObject == other.gameObject)
             {
                 target.TakeDamage(parent.Damage);
-                GameManager.Instance.Pool.ReleaseObject(gameObject);
+                myAnimator.SetTrigger("Impact");
             }
         }
     }
